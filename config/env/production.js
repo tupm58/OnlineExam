60f9507932e315cd3f4cdf4bfc88706fbf2d521a
tupm58 <kittkatt.liucuxiu@gmail.com>
2017-03-30 10:32:25 +0700

    test deploy

---
+++ config/env/production.js
@@ -0,0 +1,7 @@
+/**
+ * Created by MinhTu on 3/30/2017.
+ */
+module.exports = {
+    port: process.env.PORT || 8000,
+    db: 'mongodb://localhost/examonline' || 'mongodb://liucuxiu:moclammiu@ds145800.mlab.com:45800/oexam'
+};
