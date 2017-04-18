2f449f8c30471c1c02e3ac6a1d3d893f32990dd3
tupm58 <kittkatt.liucuxiu@gmail.com>
2017-04-18 20:11:23 +0700

    add download folder

--- controller/answerSheet.controller.js
+++ controller/answerSheet.controller.js
@@ -13,16 +13,16 @@ exports.createAnswer = function (req, res) {
     var answerSheet = new AnswerSheet(req.body);
     answerSheet.user = req.user;
     AnswerSheet.create(answerSheet)
-        .then(function(result){
+        .then(function (result) {
             console.log(1);
             res.jsonp({
                 id: result._id
             });
-        }).catch(function(err){
-            console.log(err);
+        }).catch(function (err) {
+        console.log(err);
     });
 };
-exports.getDetail = function(req,res){
+exports.getDetail = function (req, res)  {
     var id = req.params.id;
     try {
         id = mongoose.Types.ObjectId(id);
@@ -30,17 +30,17 @@ exports.getDetail = function(req,res){
         return res.status(400).send({
             message: 'Exam is invalid'
         });
-    }   
+    }
     AnswerSheet.findOne(id)
         .select('_id created_at user sheet mark exam')
-        .populate('sheet.question','_id mark answers.content answers.correct')
-        .populate('exam','_id name')
-        .then(function(result){
+        .populate('sheet.question', '_id mark answers.content answers.correct')
+        .populate('exam', '_id name')
+        .then(function (result) {
             var mark = 0;
             var totalMark = 0;
             var trueAnswer = 0;
-            result.sheet.forEach(function(sheet){
-                sheet.question.answers.forEach(function(answer){
+            result.sheet.forEach(function (sheet) {
+                sheet.question.answers.forEach(function (answer) {
                     if (answer.correct == true && answer.content == sheet.answer) {
                         mark = mark + sheet.question.mark;
                         trueAnswer++;
@@ -48,87 +48,97 @@ exports.getDetail = function(req,res){
                 });
                 totalMark = totalMark + sheet.question.mark;
             });
-            var markByPercent = mark/totalMark*100;
-            if (result.mark == undefined){
-                AnswerSheet.update({ _id : id},{
+            var markByPercent = mark / totalMark * 100;
+            if (result.mark == undefined) {
+                AnswerSheet.update({_id: id}, {
                     $set: {
-                        mark : mark,
-                        markByPercent : Math.ceil(markByPercent)
+                        mark: mark,
+                        markByPercent: Math.ceil(markByPercent)
                     }
-                }).then(function(res){
+                }).then(function (res) {
                     console.log("ok");
-                }).catch(function(err){
+                }).catch(function (err) {
                     console.log(er);
                 })
             }
             res.jsonp({
                 id: result._id,
                 exam: result.exam.name,
-                created_at : result.created_at,
+                created_at: result.created_at,
                 mark,
                 trueAnswer,
                 countQuestion: result.sheet.length,
                 totalMark,
                 markByPercent
             });
-        }).catch(function(err){
+        }).catch(function (err) {
         console.log(err);
     })
 };
 
-exports.getResultByUser = function(req,res){
-    AnswerSheet.find({'user' : req.user._id})
-        .select('_id exam created_at mark markByPercent' )
+exports.getResultByUser = function (req, res) {
+    AnswerSheet.find({'user': req.user._id})
+        .select('_id exam created_at mark markByPercent')
         .populate('exam', 'name')
         .exec()
-        .then(function(results){
+        .then(function (results) {
             res.jsonp(results)
-        }).catch(function(err){
+        }).catch(function (err) {
         return res.status(400).send({
             message: "results list error"
         });
     })
 };
 
-exports.getResultByExam = function(req,res){
+exports.getResultByExam = function (req, res) {
     var examId = req.params.examId;
-    AnswerSheet.find({ exam: examId })
-        .populate('user','_id local.username')
-        .populate('exam','name')
-        .select('_id exam user created_at mark markByPercent' )
+    AnswerSheet.find({exam: examId})
+        .populate('user', '_id local.username')
+        .populate('exam', 'name')
+        .select('_id exam user created_at mark markByPercent')
         .exec()
-        .then(function(results){
-            res.jsonp({
-                result:results
+        .then(function (results) {
+            var mark1 = results.filter(function(result){
+               return result.markByPercent >= 80;
             });
-        }).catch(function(err){
-            return res.status(400).send({
-                message: "results list error"
-            })
-        });
+            var mark2 = results.filter(function(result){
+                return (result.markByPercent < 80 && result.markByPercent >50);
+            });
+            var mark3 = results.filter(function(result){
+                return result.markByPercent <= 80;
+            });
+            res.jsonp({
+                result: results,
+                doTime : results.length
+            });
+        }).catch(function (err) {
+        return res.status(400).send({
+            message: "results list error"
+        })
+    });
 };
 
-exports.exportXls = function(req,res){
+exports.exportXls = function (req, res) {
     var examId = req.params.examId;
-    AnswerSheet.find({ exam: examId })
-        .populate('user','_id local.username')
-        .populate('exam','name')
-        .select('_id exam user created_at mark markByPercent' )
+    AnswerSheet.find({exam: examId})
+        .populate('user', '_id local.username')
+        .populate('exam', 'name')
+        .select('_id exam user created_at mark markByPercent')
         .exec()
-        .then(function(results){
+        .then(function (results) {
             console.log(results);
-            var fields = ['_id', 'user.local.username', 'exam.name','mark','markByPercent','created_at'];
-            var fieldNames = ['_id','Username','Exam','Mark','Percent','Date'];
+            var fields = ['_id', 'user.local.username', 'exam.name', 'mark', 'markByPercent', 'created_at'];
+            var fieldNames = ['_id', 'Username', 'Exam', 'Mark', 'Percent', 'Date'];
             try {
                 var opts = {
-                  data: results,
+                    data: results,
                     fields: fields,
                     fieldNames: fieldNames,
                     quotes: ' '
                 };
                 var result = json2csv(opts);
-                var path = './download/'+ examId +'.csv';
-                fs.writeFile(path, result, function(err) {
+                var path = './download/' + examId + '.csv';
+                fs.writeFile(path, result, function (err) {
                     if (err) throw err;
                     // res.download(path);
                     return res.status(200).send({
@@ -141,14 +151,14 @@ exports.exportXls = function(req,res){
                 // Be sure to provide fields if it is possible that your data array will be empty. 
                 console.error(err);
             }
-        }).catch(function(err){
-            return res.status(400).send({
-                message: "results list error"
-            })
+        }).catch(function (err) {
+        return res.status(400).send({
+            message: "results list error"
+        })
     });
 };
 
-exports.downloadResult = function(req,res){
+exports.downloadResult = function (req, res) {
     var filePath = req.params.file;
-    res.download('./download/'+ filePath);
+    res.download('./download/' + filePath);
 };
